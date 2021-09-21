e56798fb2142343776dbcf3f21b4ad2c0f5df709
Renan Fabiani <renanfabiani93@gmail.com>
2021-09-20 21:28:13 -0300

    Exercicio resolvido

---
+++ src/medidasTriangulos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class medidasTriangulos {
+
+	/*Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas válidas). Em seguida, mostrar o valore
+	 das áreas dos dois triângulos e dizer qual dos dois triângulos possui maior área.
+	 A fórmula para calcular a área de um triângulo a a partir das medidas de seus lados a, b e c é a seguinte (fórmula de Heron):
+	 area = Raiz de p(p-a) (p-b) (p-c) onde p = (a+b+c)/2
+	 */
+	
+	Scanner sc = new Scanner (System.in);
+	double 
+	
+}
